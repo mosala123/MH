a767ba4bc7cffd0a4ae3130346eef782bfda17e3
mosala123 <149291901+mosala123@users.noreply.github.com>
2024-03-30 13:26:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+      el: '.swiper-scrollbar',
+  },
+});
+
+  // swiper two 
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 3000, // milliseconds
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
+
+  // 
+// -------------------------
+ 
+// show the images 
+
+ 
+var swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
